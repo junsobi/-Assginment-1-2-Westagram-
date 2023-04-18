8b6e5a77c2b92d93cd99a8123999abc708549843
김준섭 <junsobi@gmail.com>
2023-04-18 14:11:03 +0900

    리액트로 이미그레이트완료

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Kimjunsob from "./Kimjunsob";
-import LoginPage from "./pages/Login/Login.js";
+
 import MainPage from "./pages/Main/Main.js";
 ReactDOM.render(
   <React.StrictMode>
