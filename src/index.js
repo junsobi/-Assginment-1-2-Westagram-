6851650a2a7010b66c559d7239f4b9ac3935cff5
김준섭 <junsobi@gmail.com>
2023-04-18 21:07:12 +0900

    리액트 ssas 라우터 적용완료

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import LoginPage from "./pages/Login/Login.js";
-import Main from "./pages/Main/Main.js";
+
 import Router from "./Router";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Router />);
+ReactDOM.render(<Router />, document.getElementById("root"));
